ca4f66f6691867d4da0ecf95dc54240c27a408c6
riccua001 <ricardo.cuadra@solna.se>
2023-09-06 08:07:22 +0200

    Test commit

--- src/Triangel2.java
+++ src/Triangel2.java
@@ -1,13 +1,16 @@
+import java.util.Scanner;
+
 public class Triangel2 {
     public static void main(String[] args) {
+        Scanner tgb = new Scanner(System.in);
         int a , b, c;
         double omkrets, s, area;
-        a = 10;
-        b = 5;
-        c = 6;
+        a = tgb.nextInt();
+        b = tgb.nextInt();
+        c = tgb.nextInt();
         omkrets = a+b+c;
         s = omkrets/2;
         area = Math.sqrt(s*(s-a)*(s-b)*(s-c));
-        System.out.println("Area = "+ area);
+        System.out.printf("Omkrets = %.2f \nArea = %.2f",omkrets, area);
     }
 }
